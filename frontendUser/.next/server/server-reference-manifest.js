147f8d672f2b7ed6ced8f72c146e153343e24ff2
HuynhDat2002 <hltdat2002@gmail.com>
2025-08-03 23:21:43 +0700

    -a

--- frontendUser/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"/bZj2Ml1tejuw45l6TDmsAr2ZlNkV0A14ObdpCvZsxg=\"\n}"
--- frontendUser/src/app/not-found.tsx
+++
@@ -1,7 +0,0 @@
-export default function Custom404() {
-    return (
-        <div className="font-bold text-xl flex justify-center items-center w-screen h-screen text-black">
-            <h1 className="">404 | Page Not Found</h1>
-        </div>
-    )
-}
